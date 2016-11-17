44eada634a1d42b28a49a28715030a47ab19458d
JWizerd <jwodke@outlook.com>
2016-11-17 11:23:33 -0700

    upload.php and upload validations using built in  super global array

--- admin/upload.php
+++ admin/upload.php
@@ -12,16 +12,35 @@
             <div class="col-lg-12">
                 <h1 class="page-header">
                     UPLOAD
-                    <small>Subheading</small>
+                    <small>Pictures</small>
                 </h1>
-                <ol class="breadcrumb">
-                    <li>
-                        <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                    </li>
-                    <li class="active">
-                        <i class="fa fa-file"></i> Blank Page
-                    </li>
-                </ol>
+                <?php
+                if(isset($_POST["submit"])) {
+                  $upload_errors = array(
+                    UPLOAD_ERR_OK          => "Successfully Uploaded.",
+                    UPLOAD_ERR_INI_SIZE    => "File exceeds upload_max_filesize set in php.ini",
+                    UPLOAD_ERR_FORM_SIZE   => "File exceeds Maximum upload size",
+                    UPLOAD_ERR_PARTIAL     => "File only partially uploaded.",
+                    UPLOAD_ERR_NO_FILE     => "No File Uploaded.",
+                    UPLOAD_ERR_NO_TMP_DIR  => "No Temporary Upload Folder",
+                    UPLOAD_ERR_CANT_WRITE  => "Failed to write to file disk",
+                    UPLOAD_ERR_EXTENSION   => "A PHP Extension stopped file upload",
+                  );
+
+                  $the_error = $_FILES['image']['error'];
+
+                  $message = $upload_errors[$the_error];
+                }
+                ?>
+                <div class="row">
+                  <form action="" enctype="multipart/form-data" method="post" class="col-md-4">
+                    <h3><?php if(!empty($upload_errors)){ echo $message; } ?></h3>
+                    <div class="form-group">
+                      <input type="file" name="image" class="form-control">
+                    </div>
+                    <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+                  </form>
+                </div>
             </div>
         </div>
         <!-- /.row -->
